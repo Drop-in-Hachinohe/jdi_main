b291d8a24f2c680416c98733c824c042ba9ebf6d
YuukiIshibashi <robopowerful.v@gmail.com>
2023-03-25 11:45:43 +0900

    お問合せフォーム

--- javascripts/main.js
+++ javascripts/main.js
@@ -14,4 +14,18 @@ $(document).ready(function(){
       $("#js-child_list").toggleClass("active");
     });
   }
+  $(window).scroll(function () {
+    console.log($(window).scrollTop())
+    if ($(window).width() > 769) {
+      if($(window).scrollTop() > 1) {
+        $('#js-header_main').hide(200);
+        $('#js-header').addClass("hidden");
+        $("#js-child_list").addClass("header_hidden");
+      } else {
+        $('#js-header_main').show();
+        $('#js-header').removeClass("hidden");
+        $("#js-child_list").removeClass("header_hidden");
+      }
+    }
+  });
 });
